dda895b60385fc20c6d72548121c1e43512e2f4e
Ashley Trinh <itsashley@hey.com>
2023-11-10 16:38:49 -0800

    fix(gitutils)!: remove unused code

--- src/codezip/gitutils.py
+++ src/codezip/gitutils.py
@@ -1,43 +1,22 @@
-from typing import Generator, Optional
-from pathlib import Path
+from typing import TYPE_CHECKING
 import subprocess
-import os
+
+if TYPE_CHECKING:
+    from typing import Optional, Pathlike
 
 
-def tracked_files(rootdir: Path) -> Generator[Path, None, None]:
-    """Yield all files tracked by Git in a directory."""
-
-    if not os.path.isdir(rootdir):
-        raise ValueError(f"{rootdir} is not a directory")
-
-    if not is_git_repo(rootdir):
-        raise ValueError(f"{rootdir} is not in a Git repository")
-
-    for root, dirs, files in os.walk(rootdir, topdown=True):
-        for i, d in enumerate(dirs):
-            if is_ignored_by_git(Path(root) / d, cwd=rootdir):
-                del dirs[i]
-            else:
-                yield Path(root) / d
-
-        for name in files:
-            filepath = Path(root) / name
-            if not is_ignored_by_git(filepath, cwd=rootdir):
-                yield filepath
-
-
-def is_git_repo(path: Path) -> bool:
+def is_git_repo(path: "Pathlike") -> bool:
     """Check if a path is a Git repository."""
 
     try:
-        subprocess.check_output(["git", "rev-parse", "--is-inside-work-tree"])
+        subprocess.check_output(["git", "rev-parse", "--is-inside-work-tree"], cwd=path)
     except subprocess.CalledProcessError:
         return False
 
     return True
 
 
-def is_ignored_by_git(file: Path, cwd: Optional[Path] = None) -> bool:
+def is_ignored_by_git(file: "Pathlike", cwd: "Optional[Pathlike]" = None) -> bool:
     """Check if a file is ignored by Git."""
 
     try:
